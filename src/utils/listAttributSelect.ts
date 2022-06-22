3e3f6e7bd7cd3af26b453f69550e27b1cf24a62f
gabrielseron <47740013+gabrielseron@users.noreply.github.com>
2022-06-22 13:55:36 +0200

    Add all attributs

--- src/utils/listAttributSelect.ts
+++ src/utils/listAttributSelect.ts
@@ -1,4 +1,4 @@
-export type listeTables = "user" // | "deleteduser"
+export type listeTables = "user"  | "promo" | "challenge" | "exercise" | 'results'
 
 interface attributSelectInterface
 {
@@ -9,13 +9,29 @@ interface attributSelectInterface
 const listAttributSelect: Record < listeTables, attributSelectInterface > = { //Constructs a type with a set of properties Keys of type Type. This utility can be used to map the properties of a type to another type.
     "user":
     {
-        primaryKey: `idUser`,
-        attribut: [`idUser`, `nameUser`, `mailUser`, `passUser`]
+        primaryKey: `id_user`,
+        attribut: [`id_user`, `email`, `password`, `verified_email`, `id_promo`, `is_admin`, `refresh_token`]
     },
-    // "deleteduser": {
-    //     primaryKey: `idDeletedUser`,
-    //     attribut: [`idDeletedUser`, `nameDeletedUser`, `mailDeletedUser`, `passDeletedUser`]
-    // }
+    "promo":
+    {
+        primaryKey: `id_promo`,
+        attribut: [`id_promo`, `name_promo`]
+    },
+    "challenge":
+    {
+        primaryKey: `id_challenge`,
+        attribut: [`id_challenge`, `id_promo`, `name_challenge`]
+    },
+    "exercise":
+    {
+        primaryKey: `id_exercise`,
+        attribut: [`id_exercise`, `name_exercise`, `question_exercise`, `request_exercise`, `result_exercise`, `points_exercise`, `order_exercise`, `id_challenge`]
+    },
+    "results":
+    {
+        primaryKey: `id_challenge`,
+        attribut: [`id_challenge`, `result`, `id_user`]
+    }
 };
 
 export default listAttributSelect;
